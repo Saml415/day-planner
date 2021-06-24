2700acef3a50a12bb969af1543605e384cda75e8
Sam Lloyd <bobdoe784@gmail.com>
2021-06-23 20:09:23 -0700

    Rough loop created need to list data attributes

--- Assets/script.js
+++ Assets/script.js
@@ -3,17 +3,19 @@ $('#currentDay').text(currentDay);
 var d = new Date();
 var timeBoxEl = $('.time-block')
 currentHour = d.getHours();
-console.log(timeBoxEl)
-$('.time-block')
+var dataHour = $('.time-block').attr('data-hour')
+console.log(dataHour) 
+console.log(timeBoxEl.length)
+
 // Apply past class to elements with the time-box class where the data-hour attribute is less than the current hour
 
-// Loop through each time-box el 
-// Get the data-hour attribute of time-box el (var)
+        
+for(var i = 0; i < timeBoxEl.length; i++){
+    
+}        // Loop through each time-box el 
+
 // If data-hour attribute is less than current hour
 // Apply past class to time-box
 
 
 // Apply present class to elements with the time-box class box where the data-hour attribute is equal to the current hour
-
-
-// Apply future class to elements with the time-box class box where the data-hour attribute is greater than the current hour 
