a3b9b2f0e9a07179d306801d1e2954af6a5d1098
Sam Lloyd <bobdoe784@gmail.com>
2021-06-24 12:13:23 -0700

    Conditional added

--- Assets/script.js
+++ Assets/script.js
@@ -4,15 +4,31 @@ var d = new Date();
 var timeBoxEl = $('.time-block')
 currentHour = d.getHours();
 var dataHour = $('.time-block').attr('data-hour')
-console.log(dataHour) 
+console.log(currentHour) 
 console.log(timeBoxEl.length)
+console.log(timeBoxEl)
+
+$( ".time-block" ).each(function(index) {
+    var dataHour = $(this).attr('data-hour')
+    console.log(dataHour);
+    if(dataHour < currentHour){
+        $('.time-block').addClass('past');
+    }
+    if(dataHour === currentHour){
+        $('.time-block').addClass('present');
+    }
+
+    else{
+        $('.time-block').addClass('future');
+    }
+  });
+
 
 // Apply past class to elements with the time-box class where the data-hour attribute is less than the current hour
 
         
-for(var i = 0; i < timeBoxEl.length; i++){
-    
-}        // Loop through each time-box el 
+// for(var i = 0; i < timeBoxEl.length; i++){}
+           // Loop through each time-box el 
 
 // If data-hour attribute is less than current hour
 // Apply past class to time-box
