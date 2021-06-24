5436e22d3a1e37ab25cd77c509bd6f258d36ccd6
Sam Lloyd <bobdoe784@gmail.com>
2021-06-24 14:36:34 -0700

    calendar now fully dynamic

--- Assets/script.js
+++ Assets/script.js
@@ -12,26 +12,19 @@ $( ".time-block" ).each(function(index) {
     var dataHour = $(this).attr('data-hour')
     console.log(dataHour);
     if(dataHour < currentHour){
-        $('.time-block').addClass('past');
+        $(this).addClass('past');
+        
     }
-    if(dataHour === currentHour){
-        $('.time-block').addClass('present');
+    if(dataHour == currentHour){
+        $(this).addClass('present');
+        
     }
 
-    else{
-        $('.time-block').addClass('future');
+    if(dataHour > currentHour){
+        $(this).addClass('future');
+        
     }
   });
 
 
-// Apply past class to elements with the time-box class where the data-hour attribute is less than the current hour
 
-        
-// for(var i = 0; i < timeBoxEl.length; i++){}
-           // Loop through each time-box el 
-
-// If data-hour attribute is less than current hour
-// Apply past class to time-box
-
-
-// Apply present class to elements with the time-box class box where the data-hour attribute is equal to the current hour
